acdcd6e3d61b7fe5974580cda45daf0a15c7c28a
Firmino Changani <firmino.changani@gmail.com>
2021-07-12 18:13:38 +0100

    feat: Setup store

--- ui/src/main.js
+++ ui/src/main.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import App from "./App.vue";
+import { store } from "./store";
 import { createApp } from "vue";
 import toUSDCurrency from "./plugins/toUSDCurrency";
 
@@ -7,5 +8,17 @@ axios.defaults.baseURL = process.env.VUE_APP_API;
 
 const app = createApp(App);
 
+/**
+ * Setup global filters
+ */
 app.use(toUSDCurrency);
+
+/**
+ * Setup store
+ */
+app.use(store);
+
+/**
+ * Mount the app
+ */
 app.mount("#vuejs-3-vuex-shopping-cart");
